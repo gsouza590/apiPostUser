3657e4fbd82dd68252b3b59c24768f2889a61ac5
Gabriel Souza <gsouza590@gmail.com>
2022-11-30 13:43:30 -0300

    Criacao do Projeto

---
+++ src/main/java/com/gabriel/apiUserPost/ApiUserPostApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.apiUserPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUserPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUserPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/apiUserPost/ApiUserPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.apiUserPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
