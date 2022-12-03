f1a587f99482ba1e6a9b988dd1417f2b65042f3b
Gabriel Souza <gsouza590@gmail.com>
2022-12-03 15:01:32 -0300

    User FindById and exception

--- src/main/java/com/gabriel/apiUserPost/repositories/UserRepository.java
+++ src/main/java/com/gabriel/apiUserPost/repositories/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.gabriel.apiUserPost.entities.User;
 @Repository
-public interface UserRepository extends MongoRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, String> {
 
 	
 }
--- src/main/java/com/gabriel/apiUserPost/services/UserServices.java
+++ src/main/java/com/gabriel/apiUserPost/services/UserServices.java
@@ -1,12 +1,14 @@
 package com.gabriel.apiUserPost.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.apiUserPost.entities.User;
 import com.gabriel.apiUserPost.repositories.UserRepository;
+import com.gabriel.apiUserPost.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserServices {
@@ -16,5 +18,11 @@ public class UserServices {
 	
 	public List<User> findAll(){
 		return rep.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = rep.findById(id);
+	
+		return user.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
