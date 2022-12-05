f935d7b5d4e187d115a7b6355542e9c763a5ef20
Gabriel Souza <gsouza590@gmail.com>
2022-12-05 13:41:30 -0300

    Criando o Post

--- src/main/java/com/gabriel/apiUserPost/config/DbMock.java
+++ src/main/java/com/gabriel/apiUserPost/config/DbMock.java
@@ -1,12 +1,16 @@
 package com.gabriel.apiUserPost.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.gabriel.apiUserPost.entities.Post;
 import com.gabriel.apiUserPost.entities.User;
+import com.gabriel.apiUserPost.repositories.PostRepository;
 import com.gabriel.apiUserPost.repositories.UserRepository;
 
 @Configuration
@@ -14,15 +18,25 @@ public class DbMock implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRep;
+	@Autowired 
+	private PostRepository postRep;
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRep.deleteAll();
+		postRep.deleteAll();
 		
 		User maria = new User(null,"Maria","maria@gmail.com");
 		User joao = new User(null,"Joao","joao@gmail.com");
 		User carlos = new User(null,"Carlos","carlos@gmail.com");
 	
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Viagem Sao Paulo", maria);
+		Post post2 = new Post(null, sdf.parse("23/05/2018"), "Bom dia ", "Melhor Dia", maria);
 		userRep.saveAll(Arrays.asList(maria, joao, carlos));
+		postRep.saveAll(Arrays.asList(post1,post2));
 	}
 
 }
--- src/main/java/com/gabriel/apiUserPost/entities/Post.java
+++ src/main/java/com/gabriel/apiUserPost/entities/Post.java
@@ -1,6 +1,7 @@
 package com.gabriel.apiUserPost.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,12 +18,15 @@ import lombok.Setter;
 @Setter
 @EqualsAndHashCode(of = "id")
 @Document
-public class User implements Serializable{
+public class Post implements Serializable{
+
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	private String id;
-	private String name;
-	private String email;
+	private Date date;
+	private String title;
+	private String body;
+	private User author;
 	
 }
