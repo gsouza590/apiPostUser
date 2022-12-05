d2f09daa327623654054769569359f4d85a9cbf0
Gabriel Souza <gsouza590@gmail.com>
2022-12-05 19:38:28 -0300

    consulta Query Methods

--- src/main/java/com/gabriel/apiUserPost/repositories/PostRepository.java
+++ src/main/java/com/gabriel/apiUserPost/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.gabriel.apiUserPost.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.gabriel.apiUserPost.entities.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
