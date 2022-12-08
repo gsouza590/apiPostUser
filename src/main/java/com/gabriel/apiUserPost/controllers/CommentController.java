e532b80237be0e99bb998d13d49960dc11fcabfa
Gabriel Souza <gsouza590@gmail.com>
2022-12-08 16:25:28 -0300

    Pegando comentário por Id

--- src/main/java/com/gabriel/apiUserPost/controllers/CommentController.java
+++ src/main/java/com/gabriel/apiUserPost/controllers/CommentController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +28,14 @@ public class CommentController {
 		List<CommentDTO> listDto = list.stream().map(x -> new CommentDTO(x)).collect(Collectors.toList());
 
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CommentDTO> findById(@PathVariable String id) {
+
+		Comment comment = service.findById(id);
+		CommentDTO dto = new CommentDTO(comment);
+		return ResponseEntity.ok().body(dto);
+
 	}
 }
--- src/main/java/com/gabriel/apiUserPost/services/CommentService.java
+++ src/main/java/com/gabriel/apiUserPost/services/CommentService.java
@@ -1,12 +1,14 @@
 package com.gabriel.apiUserPost.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.apiUserPost.entities.Comment;
 import com.gabriel.apiUserPost.repositories.CommentRepository;
+import com.gabriel.apiUserPost.services.exception.ObjectNotFoundException;
 
 @Service
 public class CommentService {
@@ -18,4 +20,10 @@ public class CommentService {
 	public List<Comment> findAll() {
 		return rep.findAll();
 	}
+
+
+	public Comment findById(String id) {
+		Optional<Comment> comment = rep.findById(id);
+		return comment.orElseThrow(() -> new ObjectNotFoundException("Comentário não encontrado"));
+		}
 }
